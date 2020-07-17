63bb43342ad735932620cf5ea54aa513cfaff4fa
Prezes <46129802+PanPrezess@users.noreply.github.com>
2020-07-17 22:13:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+alert ('UWAGA! Logowanie oraz Rejestracja zostaną dodane w przyszłości! (a tak dokładnie to gdy serwer discord będzie miał ok. 100 członków! Zapraszamy:                        https://discord.gg/CXwnfQx )');
+console.log('Widzę taktyczne szperańsko w konsoli ( ͡° ͜ʖ ͡°)');
